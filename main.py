43f2ad58deb541970127631f45eaa2a213b5f5d1
TaeSungKim <ssuzi58@gmail.com>
2023-03-20 17:33:11 +0900

    My Commit3 HSV 모델로 전환

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
 
 # 영상 소스 열기
-src = cv2.imread('c:/photo/picture01.jpg')
+src = cv2.imread('c:/photo/picture11.jpg')
 
 
 ## 영상처리/컴퓨터 비전 알고리즘 적용##
 dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
-
+dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
 
 
@@ -18,7 +18,7 @@ dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
 #영상 디스플레이
 cv2.imshow('src', src)
 cv2.imshow('dst1', dst1)
-
+cv2.imshow('dst2', dst2)
 
 
 

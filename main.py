89ef6f2f830dd317f861521f0d4808d558d461b3
TaeSungKim <ssuzi58@gmail.com>
2023-03-20 17:37:41 +0900

    My Commit4 2진화 기능 추가

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 
 # 영상 소스 열기
-src = cv2.imread('c:/photo/picture11.jpg')
+src = cv2.imread('c:/photo/picture88.jpg')
 
 
 ## 영상처리/컴퓨터 비전 알고리즘 적용##
 dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
 dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-
+dst3 = src.copy()
+_, dst3 = cv2.threshold(dst3, 127, 255, cv2.THRESH_BINARY)
 
 
 
@@ -19,7 +20,7 @@ dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 cv2.imshow('src', src)
 cv2.imshow('dst1', dst1)
 cv2.imshow('dst2', dst2)
-
+cv2.imshow('dst3', dst3)
 
 
 # 마무리
